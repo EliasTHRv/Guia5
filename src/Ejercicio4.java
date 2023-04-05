c4bd128ca6c512ce55cc8586762c4cf1667c645a
Elias Barreto <eliasgobarreto@gmail.com>
2023-04-05 18:58:55 -0300

    MOD Ejer4

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -2,47 +2,52 @@
 import java.util.Random;
 
 /**
- *Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y muestre la traspuesta de la matriz. 
- * La matriz traspuesta de una matriz A se denota por B y se obtiene cambiando 
- * sus filas por columnas (o viceversa).
+ * Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y
+ * muestre la traspuesta de la matriz. La matriz traspuesta de una matriz A se
+ * denota por B y se obtiene cambiando sus filas por columnas (o viceversa).
+ *
  * @author elias
  */
 public class Ejercicio4 {
+
     public static void main(String[] args) {
         int[][] matRiz = new int[4][4];
         int[][] matTrans = new int[4][4];
         Random alea = new Random(100);
-        
-        
+
         for (int i = 0; i < matRiz.length; i++)
-        {for (int j = 0; j < matRiz.length; j++)
+        {
+            for (int j = 0; j < matRiz.length; j++)
             {
-                              matRiz[i][j]=alea.nextInt(10);
+                matRiz[i][j] = alea.nextInt(-10,10);
             }
 
         }
         System.out.println("MATRIZ ORIGINAL");
         for (int i = 0; i < matRiz.length; i++)
-        {for (int j = 0; j < matRiz.length; j++)
+        {
+            for (int j = 0; j < matRiz.length; j++)
             {
-                              System.out.print("["+matRiz[i][j]+"]");
+                System.out.print("[" + matRiz[i][j] + "]");
             }
             System.out.println(" ");
         }
-        
+
         for (int i = 0; i < matRiz.length; i++)
-        {for (int j = 0; j < matRiz.length; j++)
+        {
+            for (int j = 0; j < matRiz.length; j++)
             {
-                matTrans[j][i]= matRiz[i][j];
-           }
-            
+                matTrans[j][i] = matRiz[i][j];
+            }
+
         }
         System.out.println("-----------------------");
         System.out.println("MATRIZ TRANSPUESTA");
         for (int i = 0; i < matTrans.length; i++)
-        {for (int j = 0; j < matTrans.length; j++)
+        {
+            for (int j = 0; j < matTrans.length; j++)
             {
-                              System.out.print("["+matTrans[i][j]+"]");
+                System.out.print("[" + matTrans[i][j] + "]");
             }
             System.out.println(" ");
         }
